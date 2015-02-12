80b8dd9fb472a3137233427ef8c4dfd750c3cfed
程巍巍 <littocats@gmail.com>
2015-02-12 14:40:04 +0800

    Initial Commit

---
+++ MetalImageProcessor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MetalImageProcessor
+//
+//  Created by 程巍巍 on 15/2/12.
+//  Copyright (c) 2015年 Littocats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MetalImageProcessor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MetalImageProcessor
+//
+//  Created by 程巍巍 on 15/2/12.
+//  Copyright (c) 2015年 Littocats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
